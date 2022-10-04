0c928a0906eeac9b60a41db8a18c98c362a0c9c5
batheand <batheand@gmail.com>
2022-10-04 14:07:52 +0300

    sanırsam şu an baya baya çalışıyor

--- evaluator.java
+++ evaluator.java
@@ -18,11 +18,11 @@ public class evaluator {
                 movieMatchList[i] [0] = i;
                 movieMatchList[i] [1] = 0;
                 
-            } /*else {
+            } else {
 
                 movieMatchList[i] [0] = i;
                 movieMatchList[i] [1] = matchPercentCalculator(movies, i, profiledCategories);
-            } */
+            } 
         }
         movieMatchList = sorter(movieMatchList);
         System.out.println(movies[movieMatchList[0][0]].movieName + " " +(movieMatchList[0][1] + 2));
@@ -31,18 +31,20 @@ public class evaluator {
     }
 
     public int [] [] sorter (int [] [] movieMatchList) {
-        boolean wasntSorted = false;
+        boolean wasntSorted = true;
         do {
-            wasntSorted = false;
+            wasntSorted = true;
             for (int i = 0; i<49; i++) {
                 if (movieMatchList [i][1]< movieMatchList [i+1][1]) {
-                    int temp = movieMatchList [i][1];
+                    int tempPoint = movieMatchList [i][1], tempNumber = movieMatchList [i][0];
                     movieMatchList [i][1] = movieMatchList [i+1][1];
-                    movieMatchList [i+1][1] = temp;
-                    wasntSorted = true;
+                    movieMatchList [i+1][1] = tempPoint;
+                    movieMatchList [i][0] = movieMatchList [i+1][0];
+                    movieMatchList [i+1][0] = tempNumber;
+                    wasntSorted = false;
                 }
             }
-        } while (wasntSorted);
+        } while (!wasntSorted);
         return movieMatchList;
     }
 
@@ -143,145 +145,145 @@ public class evaluator {
         movies[2].fillMovie("Fight Club", 2003, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,1,0,1,0,0,0,0,0,0,0,0,0,0};
-        movies[3].fillMovie("The Godfather", 1972, categoriesTemp, "Paramount Pictures");
+        movies[3].fillMovie("The Godfather", 1972, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,1,0,1,0,0,0,0,0,0,0,0,0,0};
-        movies[4].fillMovie("Joker", 2019, categoriesTemp, "Warner Bros.");
+        movies[4].fillMovie("Joker", 2019, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,1,0,1,0,0,0,0,0,0,0,0,0,1};
-        movies[5].fillMovie("The Batman", 2022, categoriesTemp, "Warner Bros.");
+        movies[5].fillMovie("The Batman", 2022, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,1,0,0,0,0,1,0,0,1,1};
-        movies[6].fillMovie("Cars", 2006, categoriesTemp, "Walt Disney Pictures");
+        movies[6].fillMovie("Cars", 2006, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,1,0,0,0,0,1,1,0,1,1};
-        movies[7].fillMovie("Frozen", 2013, categoriesTemp, "Walt Disney Pictures");
+        movies[7].fillMovie("Frozen", 2013, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,1,0,0,0,0,0,0,0,1,1};
-        movies[8].fillMovie("Space Jam", 2003, categoriesTemp, "Warner Bros.");
+        movies[8].fillMovie("Space Jam", 2003, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,1,0,0,1,0,1,0,0,0,0,1,0};
-        movies[9].fillMovie("It", 2017, categoriesTemp, "Warner Bros.");
+        movies[9].fillMovie("It", 2017, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,1,1,0,1,0,0,0,0,0,1,0,0};
-        movies[10].fillMovie("Gravity", 2013, categoriesTemp, "Warner Bros.");
+        movies[10].fillMovie("Gravity", 2013, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,1,0,0,0,0,1,0,0,1,1};
-        movies[11].fillMovie("Monsters Inc.", 2001, categoriesTemp, "Walt Disney Pictures");
+        movies[11].fillMovie("Monsters Inc.", 2001, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,1,0,0,0,0,1,0,0,1,0};
-        movies[12].fillMovie("Ratatouille", 2007, categoriesTemp, "Walt Disney Pictures");
+        movies[12].fillMovie("Ratatouille", 2007, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,1,0,0,0,0,0,0,0,0,0,0};
-        movies[13].fillMovie("The Social Network", 2010, categoriesTemp, "Sony Pictures");
+        movies[13].fillMovie("The Social Network", 2010, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,1,0,0,0,0,0,0,0,0,0,0};
-        movies[14].fillMovie("12 Angry Men", 1957, categoriesTemp, "MGM");
+        movies[14].fillMovie("12 Angry Men", 1957, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,1,0,0,1,0,0,0,0,0,1,0,0};
-        movies[15].fillMovie("The Terminator", 1984, categoriesTemp, "MGM");
+        movies[15].fillMovie("The Terminator", 1984, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,1,0,0,0,0,1,0,0,1,1};
-        movies[16].fillMovie("Toy Story", 1995, categoriesTemp, "Walt Disney Pictures");
+        movies[16].fillMovie("Toy Story", 1995, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,0,0,1,0,0,0,0,1,1,1};
-        movies[17].fillMovie("E.T.", 1982, categoriesTemp, "Universal Picture");
+        movies[17].fillMovie("E.T.", 1982, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,1,0,0,0,0,1,0,0,1,1};
-        movies[18].fillMovie("Finding Nemo", 2003, categoriesTemp, "Walt Disney Pictures");
+        movies[18].fillMovie("Finding Nemo", 2003, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,1,1,1,1,0,0,0,0,0,0,0,0};
-        movies[19].fillMovie("Parasite", 2019, categoriesTemp, "Neon");
+        movies[19].fillMovie("Parasite", 2019, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,0,0,0,1,0,0,0,1,0,0};
         movies[20].fillMovie("Alien", 1979, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,0,0,1,0,0,0,1,0,1,0};
-        movies[21].fillMovie("The Wizard Of Oz", 1939, categoriesTemp, "MGM");
+        movies[21].fillMovie("The Wizard Of Oz", 1939, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,1,0,1,0,0,0,0,1,0,1};
-        movies[22].fillMovie("Back To The Future", 1985, categoriesTemp, "Universal Studios");
+        movies[22].fillMovie("Back To The Future", 1985, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,1,1,0,1,0,0,0,0,0,0,0,1};
-        movies[23].fillMovie("Jaws", 1975, categoriesTemp, "Universal Studios");
+        movies[23].fillMovie("Jaws", 1975, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,1,1,1,1,1,0,0,0,0,0,0,0,0};
-        movies[24].fillMovie("Knives Out", 2019, categoriesTemp, "Lionsgate");
+        movies[24].fillMovie("Knives Out", 2019, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,1,0,1,0,0,0,0,0,1,0};
-        movies[25].fillMovie("Charlie and the Chocolate Factory", 2005, categoriesTemp, "Warner Bros.");
+        movies[25].fillMovie("Charlie and the Chocolate Factory", 2005, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,0,0,0,0,1,0,0,0,0,0,0,1};
-        movies[26].fillMovie("Black Panther ", 2018, categoriesTemp, "Marvel Studios");
+        movies[26].fillMovie("Black Panther ", 2018, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,1,0,1,1,0,0,0,0,0,0,0,0,0};
-        movies[27].fillMovie("Catch Me If You Can", 2002, categoriesTemp, "DreamWorks");
+        movies[27].fillMovie("Catch Me If You Can", 2002, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,1,1,1,0,1,0,0,0,0,0,0,0,0};
-        movies[28].fillMovie("Night Crawler", 2014, categoriesTemp, "Open Road");
+        movies[28].fillMovie("Night Crawler", 2014, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,0,0,0,0,1,0,0,0,0,1,0,1};
-        movies[29].fillMovie("Superman: The Movie", 1978, categoriesTemp, "Warner Bros.");
+        movies[29].fillMovie("Superman: The Movie", 1978, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,1,1,1,0,1,0,0,0,0,0,0,0,0};
-        movies[30].fillMovie("The Silence of the Lambs", 1991, categoriesTemp, "Orion Pictures");
+        movies[30].fillMovie("The Silence of the Lambs", 1991, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,1,1,0,0,0,0,0,0,0,0,0};
-        movies[31].fillMovie("The Truman Show", 1998, categoriesTemp, "Paramount Pictures");
+        movies[31].fillMovie("The Truman Show", 1998, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,1,0,0,0,0,0,0,1,1,1};
-        movies[32].fillMovie("Wall-E", 2008, categoriesTemp, "Walt Disney Pictures");
+        movies[32].fillMovie("Wall-E", 2008, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,1,1,0,0,0,1,0,0,0,0,0};
-        movies[33].fillMovie("Her", 2014, categoriesTemp, "Warner Bros.");
+        movies[33].fillMovie("Her", 2014, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,0,0,0,0,1,0,0,0,0,0,0,1};
-        movies[34].fillMovie("Iron Man ", 2008, categoriesTemp, "Marvel Studios");
+        movies[34].fillMovie("Iron Man ", 2008, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,0,0,0,0,1,0,0,0,0,0,0,1};
         movies[35].fillMovie("Logan", 2017, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,1,0,0,1,0,0,0,0,0,0,0,1};
-        movies[36].fillMovie("Mission: Impossible Rogue Nation", 2015, categoriesTemp, "Paramount Pictures");
+        movies[36].fillMovie("Mission: Impossible Rogue Nation", 2015, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,0,0,0,0,1,0,0,0,0,1,0,1};
-        movies[37].fillMovie("Star Trek", 2009, categoriesTemp, "Paramount Pictures");
+        movies[37].fillMovie("Star Trek", 2009, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,0,0,0,0,1,0,0,0,0,1,0,1};
         movies[38].fillMovie("Star Wars: The Empire Strikes Back", 1980, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,0,0,0,0,1,0,0,0,0,0,0,1};
-        movies[39].fillMovie("The Dark Knight", 2008, categoriesTemp, "Warner Bros.");
+        movies[39].fillMovie("The Dark Knight", 2008, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,1,0,0,1,0,1,0,0,0,1,0,1};
         movies[40].fillMovie("The War For the Planet of the Apes", 2017, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,1,0,0,1,0,0,0,0,0,0,1};
-        movies[41].fillMovie("Hugo", 2011, categoriesTemp, "Paramount Pictures");
+        movies[41].fillMovie("Hugo", 2011, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,1,0,0,1,0,1,0,0,0,0,0,0};
-        movies[42].fillMovie("Us", 2019, categoriesTemp, "Universal Pictures");
+        movies[42].fillMovie("Us", 2019, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,1,0,0,1,0,0,0,0,0,0,0,0};
-        movies[43].fillMovie("Baby Driver", 2017, categoriesTemp, "Sony Pictures");
+        movies[43].fillMovie("Baby Driver", 2017, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,0,0,0,0,1,0,0,1,0,0};
-        movies[44].fillMovie("Eternal Sunshine of the Spotless Mind", 2004, categoriesTemp, "Focus Features");
+        movies[44].fillMovie("Eternal Sunshine of the Spotless Mind", 2004, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,1,0,0,1,0,0,0,0,0,0,0,1};
-        movies[45].fillMovie("Jurassic Park", 1993, categoriesTemp, "Universal Pictures");
+        movies[45].fillMovie("Jurassic Park", 1993, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {1,0,0,0,1,0,0,0,0,0,0,1,0,0};
-        movies[46].fillMovie("Men In Black", 1997, categoriesTemp, "Columbia Pictures");
+        movies[46].fillMovie("Men In Black", 1997, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,1,0,1,0,0,0,0,0,0,0,0,0,0};
-        movies[47].fillMovie("Pulp Fiction", 1994, categoriesTemp, "Miramax Films");
+        movies[47].fillMovie("Pulp Fiction", 1994, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,1,1,0,0,0,1,0,1,0,0,0};
-        movies[48].fillMovie("La La Land ", 2016, categoriesTemp, "Lionsgate");
+        movies[48].fillMovie("La La Land ", 2016, categoriesTemp, "20th Century Fox");
         
         categoriesTemp = new int [] {0,0,0,0,0,0,1,0,0,0,0,0,0,1};
-        movies[49].fillMovie("The Lord of the Rings: The Fellowship of the Ring", 2001, categoriesTemp, "Warner Bros.");    
+        movies[49].fillMovie("The Lord of the Rings: The Fellowship of the Ring", 2001, categoriesTemp, "20th Century Fox");    
 
         return movies;
     }
